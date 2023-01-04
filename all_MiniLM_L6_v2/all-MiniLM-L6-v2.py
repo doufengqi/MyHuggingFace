43312c3bce7045d2a4d5b4be71b15a1d358f1b24
达芬奇 <doufengqi@outlook.com>
2023-01-04 15:31:19 +0800

    milvus start

--- all_MiniLM_L6_v2/all-MiniLM-L6-v2.py
+++ all_MiniLM_L6_v2/all-MiniLM-L6-v2.py
@@ -44,46 +44,100 @@ from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Colle
 # 连接milvus
 connections.connect(host='127.0.0.1', port='19530')  # host为milvus的ip地址，port为milvus的端口号
 
-# 创建collection
-collection_name = 'sentence_embeddings'
+# 创建collection 用于存储句子向量
+collection_name = 'sentence_embedding'
 fields = [
-    FieldSchema(name="sentence", dtype=DataType.STRING, is_primary=True),
-    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=768)
+    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=384)  # 句子向量 384维
 ]
-schema = CollectionSchema(fields, description="sentence embeddings")
+schema = CollectionSchema(fields, description="sentence embedding")  # 创建collection 用于存储句子向量
 collection = Collection(name=collection_name, schema=schema)
 
-# 插入sentence_embeddings
-sentence_embeddings = sentence_embeddings.numpy().tolist()
-sentence_embeddings = [[sentences[i], sentence_embeddings[i]] for i in range(len(sentences))]
-collection.insert(sentence_embeddings)
-#将id
-
-###########################################################################################
-# 保存到文件
-with open('sentence_embeddings.txt', 'w', encoding='utf-8') as f:
-    for i in range(len(sentences)):
-        f.write(sentences[i] + '\t' + str(sentence_embeddings[i][1]) + '\n')
-
-# 保存到mysql
-import pymysql
-
-# 连接mysql
-db = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='sentence_embeddings',
-                     charset='utf8')
-cursor = db.cursor()
-
-# 创建表
-sql = 'create table sentence_embeddings (sentence varchar(255), embedding varchar(255))'
-cursor.execute(sql)
-db.commit()
-
 # 插入数据
-for i in range(len(sentences)):
-    sql = 'insert into sentence_embeddings (sentence, embedding) values (%s, %s)'
-    cursor.execute(sql, (sentences[i], str(sentence_embeddings[i][1])))
-db.commit()
+collection.insert([sentence_embeddings.tolist()])
+print("insert data into milvus")
 
-# 关闭连接
-cursor.close()
-db.close()
+# 从milvus中检索句子
+from pymilvus import connections, Collection, utility
+
+# 连接milvus
+connections.connect(host='127.0.0.1', port='19530')  # host为milvus的ip地址，port为milvus的端口号
+
+# 创建collection 用于存储句子向量
+collection_name = 'sentence_embedding'
+collection = Collection(name=collection_name)
+
+# 检索句子
+query = '到底该怎么定义'
+query_embedding = model(**tokenizer(query, padding=True, truncation=True, return_tensors='pt'))[0][0]
+query_embedding = F.normalize(query_embedding, p=2, dim=1)
+query_embedding = query_embedding.tolist()
+print("query embedding:")
+print(query_embedding)
+#
+# # 检索
+# # 向量检索
+# top_k = 5
+# search_params = {"metric_type": "L2", "params": {"nprobe": 10}}  # 表示 L2 距离，nprobe 表示搜索时的探测次数
+# # search参数分别
+# results = collection.search(query_embedding, "embedding", search_params, top_k)
+
+
+
+
+
+
+
+
+
+
+#
+#
+# # 将id和sentence对应起来存入mysql
+# import pymysql
+#
+# # 连接mysql
+# db = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='sentence_embeddings',
+#                      charset='utf8')
+# cursor = db.cursor()
+#
+# # 创建表
+# sql = 'create table sentence_embeddings2(id int primary key auto_increment, sentence varchar(255), embedding varchar(255))'
+# cursor.execute(sql)
+#
+# # 将sentence_embeddings中的数据插入到mysql中
+# sql = 'insert into sentence_embeddings(sentence, embedding) values(%s, %s)'
+# for i in range(len(sentences)):
+#     cursor.execute(sql, (sentences[i], str(sentence_embeddings[i][1])))
+# db.commit()
+# db.close()
+
+#
+# ###########################################################################################
+# # 保存到文件
+# with open('sentence_embeddings.txt', 'w', encoding='utf-8') as f:
+#     for i in range(len(sentences)):
+#         f.write(sentences[i] + '\t' + str(sentence_embeddings[i][1]) + '\n')
+#
+# # 保存到mysql
+# import pymysql
+#
+# # 连接mysql
+# db = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='123456', db='sentence_embeddings',
+#                      charset='utf8')
+# cursor = db.cursor()
+#
+# # 创建表
+# sql = 'create table sentence_embeddings (sentence varchar(255), embedding varchar(255))'
+# cursor.execute(sql)
+# db.commit()
+#
+# # 插入数据
+# for i in range(len(sentences)):
+#     sql = 'insert into sentence_embeddings (sentence, embedding) values (%s, %s)'
+#     cursor.execute(sql, (sentences[i], str(sentence_embeddings[i][1])))
+# db.commit()
+#
+# # 关闭连接
+# cursor.close()
+# db.close()
