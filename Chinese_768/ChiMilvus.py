9ac321b96e9e3693727902d462551530c649e261
达芬奇 <doufengqi@outlook.com>
2023-01-05 21:07:56 +0800

    mvilvus实现向量添加实现自主id

--- Chinese_768/ChiMilvus.py
+++ Chinese_768/ChiMilvus.py
@@ -5,21 +5,39 @@ from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Colle
 connections.connect(host='127.0.0.1', port='19530')  # host为milvus的ip地址，port为milvus的端口号
 
 # 创建collection
-collection_name = 'test_collection'
+collection_name = 'c_talk_test'
 fields = [
-    FieldSchema(name="int64", dtype=DataType.INT64, is_primary=True, auto_id=False),
+    # 从1开始生成id
+    FieldSchema(name="int64", dtype=DataType.INT64, is_primary=True, auto_id=False),  # id 为主键 且为int64类型 且为自增
     FieldSchema(name="float", dtype=DataType.FLOAT_VECTOR, dim=768),
 ]
-schema = CollectionSchema(fields=fields, description="test collection")
+schema = CollectionSchema(fields=fields, description="c_talk_test")
 collection = Collection(name=collection_name, schema=schema)
 
-with open('sentence_embeddings.csv', 'r', encoding='utf-8') as f:
+with open('../data/out/chiOut.csv', 'r', encoding='utf-8') as f:
+    # 读取csv第一行第一列和第一列最后一行存于begin和end中
     reader = csv.reader(f)
-    for i, row in enumerate(reader):
-        # 强制转换为int64
-        row[0] = int(row[0])
-        # 强制转换为float_vector
-        row[2] = eval(row[2])
-        # 插入数据到milvus
-        collection.insert([[row[2]]])
-        print(i, row[1])
+    my_vector = []
+    # begin = next(reader)[0]
+    for row in reader:
+        if row[0] == '1':
+            begin = row[0]
+        my_vector.append(eval(row[2]))
+    end = row[0]
+
+    # 生成list 从begin到end
+    my_id = [i for i in range(int(begin), int(end) + 1)]
+
+    # 插入数据
+    collection.insert([my_id, my_vector])
+
+# 建立索引
+# collection = Collection("c_talk_test")  # Get an existing collection.
+# collection.create_index(
+#     field_name="float",
+#     index_params={
+#         "metric_type": "L2",
+#         "index_type": "IVF_FLAT",
+#         "params": {"nlist": 1024}
+#     }
+# )
--- Chinese_768/chi_mod.py
+++ Chinese_768/chi_mod.py
@@ -1,3 +1,4 @@
+import csv
 from text2vec import SentenceModel
 
 sentences = []
@@ -8,3 +9,9 @@ with open('../data/sentences.txt', 'r', encoding='utf-8') as f:
 model = SentenceModel('shibing624/text2vec-base-chinese')
 embeddings = model.encode(sentences)
 print(embeddings)
+
+
+with open('../data/out/chiOut.csv', 'w', encoding='utf-8', newline='') as f:
+    writer = csv.writer(f)
+    for i, sentence in enumerate(sentences):
+        writer.writerow([i + 1, sentence, embeddings[i].tolist()])
